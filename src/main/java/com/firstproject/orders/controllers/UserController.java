23b8aa579a40301339fa861fb4185da09927c20a
VictorEdM <thomasmannautor@gmail.com>
2022-09-22 15:32:12 -0300

    User update

--- src/main/java/com/firstproject/orders/controllers/UserController.java
+++ src/main/java/com/firstproject/orders/controllers/UserController.java
@@ -45,4 +45,10 @@ public class UserController {
         return ResponseEntity.noContent().build();
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+        User userUpdated = service.update(id, user);
+        return ResponseEntity.ok().body(userUpdated);
+    }
+
 }
--- src/main/java/com/firstproject/orders/services/UserService.java
+++ src/main/java/com/firstproject/orders/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class UserService {
@@ -27,5 +28,29 @@ public class UserService {
 
     public void delete(Long id) {
         repository.deleteById(id);
+    }
+
+    public User update(Long id, User userUpdated) {
+        User currentUser = repository.getReferenceById(id);
+        updateData(currentUser, userUpdated);
+        return repository.save(currentUser);
+    }
+
+    public void updateData(User oldUser, User newUser) {
+        if (Objects.nonNull(newUser.getName())) {
+            oldUser.setName(newUser.getName());
+        }
+
+        if (Objects.nonNull(newUser.getEmail())) {
+            oldUser.setEmail(newUser.getEmail());
+        }
+
+        if (Objects.nonNull(newUser.getPhone())) {
+            oldUser.setPhone(newUser.getPhone());
+        }
+
+        if (Objects.nonNull(newUser.getPassword())) {
+            oldUser.setPassword(newUser.getPassword());
+        }
     }
 }
