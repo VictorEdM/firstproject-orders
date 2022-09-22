f6c3cc71e011e91af946379bbf356ed3dd97dcf9
VictorEdM <thomasmannautor@gmail.com>
2022-09-21 21:03:15 -0300

    OrderItem and OrderItemPK

---
+++ src/main/java/com/firstproject/orders/entities/OrderItem.java
@@ -0,0 +1,52 @@
+package com.firstproject.orders.entities;
+
+import com.firstproject.orders.entities.pk.OrderItemPK;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @EmbeddedId
+    private OrderItemPK id;
+
+    private Integer quantity;
+    private BigDecimal price;
+
+    public OrderItem(Order order, Product product, Integer quantity, BigDecimal price) {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Order getOrder() {
+        return this.id.getOrder();
+    }
+
+    public void setOrder(Order order) {
+        this.id.setOrder(order);
+    }
+
+    public Product getProduct() {
+        return this.id.getProduct();
+    }
+
+    public void setProduct(Product product) {
+        this.id.setProduct(product);
+    }
+
+}
---
+++ src/main/java/com/firstproject/orders/entities/pk/OrderItemPK.java
@@ -0,0 +1,39 @@
+package com.firstproject.orders.entities.pk;
+
+import com.firstproject.orders.entities.Order;
+import com.firstproject.orders.entities.Product;
+import lombok.Data;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Data
+@Embeddable
+public class OrderItemPK implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof OrderItemPK that)) return false;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
+    }
+}
